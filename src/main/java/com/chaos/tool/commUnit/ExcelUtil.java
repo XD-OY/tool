78b79fd747be687b5e44072a88b85f69d4421c47
XD_OY <chaouyany@163.com>
2018-12-24 17:30:46 +0800

    增加jar包

---
+++ src/main/java/com/chaos/tool/commUnit/ExcelUtil.java
@@ -0,0 +1,21 @@
+package com.chaos.tool.commUnit;
+
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.excel.metadata.Sheet;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class ExcelUtil {
+
+    //easyExcel的用法
+    //读取-写入-下载
+    //见有道
+
+    private InputStream inputStream;
+    private Sheet sheet;
+    //List<Object> data = EasyExcelFactory.read(inputStream, sheet);
+    //List<Object> data = EasyExcelFactory.read(inputStream, new Sheet(2, 1,JavaModel.class));
+
+
+}
